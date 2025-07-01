c073deb18138d1e1800b827e411172f6d8044454
thugony <theo.hugony@ert-energy.com>
2025-07-01 21:43:36 +0200

    Update PURGE_UNUSED.ps1

--- ertforms/PURGE_UNUSED.ps1
+++ ertforms/PURGE_UNUSED.ps1
@@ -1,10 +1,7 @@
 Write-Host "Nettoyage du projet Flutter pour Android uniquement..."
 
 # Supprimer les dossiers inutiles
-Remove-Item -Recurse -Force .dart_tool, .idea, .vscode, build, ios, linux, macos, windows, web, test
-
-# Supprimer les fichiers inutiles
-Remove-Item -Force analysis_options.yaml, ertforms.iml
+Remove-Item -Recurse -Force .dart_tool, test
 
 # Nettoyage et récupération des packages
 flutter clean
